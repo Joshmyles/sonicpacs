5d34c8d46785dbcee50bcf4756e04e983e181a74
Josh <48190147+Joshmyles@users.noreply.github.com>
2023-11-04 19:49:03 +0300

    Update HeroImage.jsx

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,7 +2,7 @@ const HeroImage = () => {
   return (
     <>
       <div className="flex w-full mx-auto pt-8 lg:w-4/5">
-        <img src="src/assets/images/MR.png" alt="System Image" />
+        <img src="sonicpacs/src/assets/images/MR.png" alt="System Image" />
       </div>
     </>
   );
